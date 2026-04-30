24c16c29ff719073cd50cce072264b5625c448c1
smartgera1990 <nnaghageraldine@yahoo.com>
2026-04-29 22:50:34 -0500

    Add comments to generate_transactions and run_shieldmark

--- src/run_shieldmark.py
+++ src/run_shieldmark.py
@@ -1,18 +1,24 @@
+#we call process transaction and generate transcations
 from validate_amount import process_transaction
 from generate_transactions import generate_transactions
 
-generate_transactions()
-# print (transactions)
+#generate transactions is called first so that we can get the actual and fraudulent transactions
+transactions = generate_transactions()
 
+
+
+#build the user front interface report that the small business owner sees
 print("====================================")
 print("   SHIELDMARK TRANSACTION REPORT")
 print("====================================")
 
-for transaction in generate_transactions():
+#call all transactions in generate_transaction
+for transaction in transactions:
+    #tranfer all those transcations to process_transaction
     result = process_transaction(transaction)
 
     
-
+    #display all employee transaction record to the owner of the business
     print("Employee: " + result["staff_name"] ) 
     print("Discrepancy: $" + str(result["discrepancy"]))
     print("Status: " + result["status"]) 
